8b5ad0132e46f33d8ac706dd7d5b1a00c1997e84
devshehan <shehaan.avishka00@gmail.com>
2023-07-06 07:29:03 +0530

    typedef

---
+++ learning/typedefMacros.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<pair<int,string>> vp;
+
+int main(){
+
+    int a;
+    string name;
+
+    cin >> a >> name;
+
+    vp x;
+    x.push_back(make_pair(a,name));
+
+    return 0;
+}
