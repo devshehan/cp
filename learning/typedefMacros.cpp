d98784667961782aa01cf05b5a4d5f38ea2e2ffb
devshehan <shehaan.avishka00@gmail.com>
2023-07-06 07:44:48 +0530

    macros and typedef ++

--- learning/typedefMacros.cpp
+++ learning/typedefMacros.cpp
@@ -6,6 +6,11 @@ typedef vector<int> vi;
 typedef vector<string> vs;
 typedef vector<pair<int,string>> vp;
 
+#define mp make_pair
+#define pb push_back
+
+#define loopo(a) for(int i =0; i< a; ++i)
+
 int main(){
 
     int a;
@@ -14,7 +19,14 @@ int main(){
     cin >> a >> name;
 
     vp x;
-    x.push_back(make_pair(a,name));
+    x.pb(mp(a,name));
+
+
+    loopo(10){
+        cout << x[0].first << endl;
+        cout << x[0].second << endl; 
+    }
+
 
     return 0;
 }
